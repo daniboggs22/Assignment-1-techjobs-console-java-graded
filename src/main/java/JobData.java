5c082dece18c28d1aa906727118c41cefe24e864
Daneille Boggs <daniboggs22@gmail.com>
2023-01-20 10:03:08 -0600

    Closer to getting findByValue method pass test.

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -5,10 +5,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -103,10 +100,10 @@ public class JobData {
         for (HashMap<String, String> column : allJobs) {
             //loop through columns to check for searchTerm
             for (String i: column.values()){
-               if(column.containsValue(value)){
-                  jobs.add(column);
-               } else if(jobs.contains(column)){
-                   break;
+               if(jobs.contains(column)){
+
+               } else if(column.containsValue(value)){
+                   jobs.add(column);
                }
 
                 }
