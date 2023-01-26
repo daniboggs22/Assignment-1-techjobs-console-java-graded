5565e6e5ae141fb279b5d22f4bc8adaed8c09979
Daneille Boggs <daniboggs22@gmail.com>
2023-01-26 13:38:55 -0600

    Updated methods to case insensitive.

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -76,7 +76,7 @@ public class JobData {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.toLowerCase().contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
@@ -100,6 +100,8 @@ public class JobData {
         for (HashMap<String, String> column : allJobs) {
             //loop through each column to check for searchTerm value
             for (String aValue: column.values()){
+                aValue.toLowerCase();
+                value.toLowerCase();
                 //if column contains the searchTerm value & not already included in the jobs arrayList
                 //add it to the jobs arrayList
                if(column.containsValue(value) && !jobs.contains(column)){
